a8285aac1614d221b3b82050f884bf14a9c80214
Caio Barbieri <caiolvbarbieri@pm.me>
2021-09-10 22:56:58 -0300

    Botões e Navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,31 @@
 // Smooth Scrolling
 $("#navbar a, .btn").on("click", function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
+	if (this.hash !== "") {
+		event.preventDefault();
 
-        const hash = this.hash;
+		const hash = this.hash;
 
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+	}
 });
 
 // Sticky menu background
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
-    } else {
-        document.querySelector("#navbar").style.opacity = 1;
-    }
+	if (window.scrollY > 150) {
+		document.querySelector("#navbar").style.opacity = 0.9;
+	} else {
+		document.querySelector("#navbar").style.opacity = 1;
+	}
+});
+
+// Alert
+$(document).ready(function () {
+	$('.btn-hover.color-9').click(function () {
+		$('.alert').show()
+	})
 });
